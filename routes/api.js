69bbd6011689936641dbf46fab1178b0199abbf9
GuidoFlores <gjflores@espol.edu.ec>
2024-07-15 09:57:49 -0500

    update

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const itemController = require('../controllers/itemController');
 
 router.post('/items', itemController.createItem);
-router.get('/items', itemController.getItem);
+router.get('/items', itemController.getAllItems);
 router.get('/items/:id', itemController.getItem);
 router.put('/items/:id', itemController.updateItem);
 router.delete('/items/:id', itemController.deleteItem);

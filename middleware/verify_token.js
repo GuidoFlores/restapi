04a390d2cca05fda60236488c512160499e392f7
GuidoFlores <gjflores@espol.edu.ec>
2024-08-14 21:45:25 -0500

    update

---
+++ middleware/verify_token.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+
+SECRET_KEY = 'shhhhhhhhhhhh'
+
+// Middleware para verificar el token JWT
+const verifyToken = (req, res, next) => {
+    // Obtener el token del encabezado de autorización
+    const token = req.headers['authorization'];
+
+    // Verificar si el token existe
+    if (!token) {
+        return res.status(408).json({ error: 'Token no proporcionado' });
+    }
+
+    // Verificar la validez del token
+    jwt.verify(token,SECRET_KEY, (err, decoded) => {
+        if (err) {
+            return res.status(408).json({ error: 'Token inválido o expirado' });
+        }
+
+        // Token válido, guardar la información decodificada en la solicitud y continuar
+        req.user = decoded;
+        next();
+    });
+};
+
+module.exports = verifyToken;
--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const itemController = require('../controllers/itemController');
+const verifyToken = require('./middleware/verify_token'); 
 
-router.get('/items/:id/', itemController.verifyToken)
+router.use('/api', verifyToken);
+
+router.get('/api/items/:id', async (req, res) => {
+    try {
+        const itemId = req.params.id;
+        const itemDoc = await db.collection('items').doc(itemId).get();
+        
+        if (!itemDoc.exists) {
+            return res.status(404).json({ error: 'Item no encontrado' });
+        }
+
+        res.status(200).json({ id: itemDoc.id, ...itemDoc.data() });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
 router.post('/items', itemController.createItem);
 router.get('/items', itemController.getAllItems);
 router.get('/items/:id', itemController.getItem);
-
-//
-
-
 router.put('/items/:id', itemController.updateItem);
 router.delete('/items/:id', itemController.deleteItem);
 

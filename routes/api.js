c99e11432b61837ea85aefe2da6b85ab60a162db
GuidoFlores <gjflores@espol.edu.ec>
2024-08-14 22:25:02 -0500

    update

--- routes/api.js
+++ routes/api.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const itemController = require('../controllers/itemController');
-const verifyToken = require('./middleware/verifyToken');
+const verifyToken = require('../middleware/verifyToken');
 
 
 router.use('/api', verifyToken);
 
-router.post('/api/items', itemController.createItem, verifyToken.verifyToken);
-router.get('/api/items', itemController.getAllItems,verifyToken.verifyToken);
-router.get('/api/items/:id', itemController.getItem,verifyToken.verifyToken);
+router.post('/api/items', itemController.createItem, verifyToken);
+router.get('/api/items', itemController.getAllItems,verifyToken);
+router.get('/api/items/:id', itemController.getItem,verifyToken);
 
 //
 
 
-router.put('/api/items/:id', itemController.updateItem,verifyToken.verifyToken);
-router.delete('/api/items/:id', itemController.deleteItem,verifyToken.verifyToken);
+router.put('/api/items/:id', itemController.updateItem,verifyToken);
+router.delete('/api/items/:id', itemController.deleteItem,verifyToken);
 
 module.exports = router;
